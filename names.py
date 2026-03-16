86e31d812b9e4874ec33ae83b47debd148947d5e
baltazardiez7-lab <baltazardiez7@gmail.com>
2026-03-16 18:43:28 -0300

    Update names.py

--- names.py
+++ names.py
@@ -8,4 +8,4 @@ def names():
     print (nombre.lower() +" "+ apellido.lower())
     print(nombre.title() + " " + apellido.title())
     print(nombre.upper() + " " + apellido.upper())
-    print(f" \t{nombre.lower()} {apellido.lower()} ")
+    print(f"\t{nombre.lower()} {apellido.lower()} ")
