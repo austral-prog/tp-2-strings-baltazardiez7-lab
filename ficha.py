18f6480e9966608a5e08c3aaad33677f0d77dcf3
baltazardiez7-lab <baltazardiez7@gmail.com>
2026-03-17 12:27:40 -0300

    Update ficha.py

--- ficha.py
+++ ficha.py
@@ -43,7 +43,7 @@ def ficha():
     name=nombre[0:espacio]
     surname=nombre[espacio+1:]
     print (f"Usuario: {surname.lower()}.{name.lower()}")
-    print (f"Email Valido: {'@'in email}")
+    print (f"Email valido: {'@'in email}")
     arroba= int(email.find("@"))
     print (f"Dominio: {email.lower()[arroba+1:]}")
     print (f"Nombre para archivo: {name.title()}_{surname.title()}")
