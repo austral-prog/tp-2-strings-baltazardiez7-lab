6f4e829a3f562bff3d45ddb85b025721063dac25
baltazardiez7-lab <baltazardiez7@gmail.com>
2026-03-17 10:27:31 -0300

    Update string_methods.py

--- string_methods.py
+++ string_methods.py
@@ -6,8 +6,8 @@ def string_methods():
     nombre = "   Grace Hopper   "
     frase = "Python es un gran lenguaje de programacion"
     multilinea = """Linea 1
-    Linea 2
-    Linea 3"""
+Linea 2
+Linea 3"""
     print (f"Strip: {nombre.strip()}")
     print (f"Lstrip: {nombre.lstrip()}")
     print (f"Rstrip: {nombre.rstrip()}")
@@ -15,7 +15,7 @@ def string_methods():
     print (f"Lower: {frase.lower()}")
     print (f"Title: {frase.title()}")
     print (f"Find: {frase.find('gran')}")
-    print (f"Replace {frase.replace('programacion', 'desarrollo')}")
+    print (f"Replace: {frase.replace('programacion', 'desarrollo')}")
     print (f"Count: {frase.count('a')}")
     print (f"Contiene Python: {'Python' in frase}")
     print (f"Contiene Java: {'Java' in frase}")
