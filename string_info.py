b94095c1247f9f0bdb9822478f8819c91efa8b67
baltazardiez7-lab <baltazardiez7@gmail.com>
2026-03-17 09:55:46 -0300

    Update string_info.py

--- string_info.py
+++ string_info.py
@@ -4,7 +4,7 @@ def string_info():
     """
     palabra = "Programacion"
     print (f"Palabra: {palabra}")
-    print (f"Longitud {len(palabra)}")
+    print (f"Longitud: {len(palabra)}")
     print (f"Primera Letra: {palabra[0]}")
     print (f"Ultima Letra: {palabra[-1]}")
     print (f"Repetida: {palabra*3}")
